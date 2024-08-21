cd9b3f1e1171061b0d980613ee17720b7444db8d
Brendon Lees <brendon1555@gmail.com>
2024-08-21 16:46:18 +1000

    Clean up boilerplate and add deps we will need

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxy the requests to the Flickr API as Flickr does not support CORS
+      "/images": {
+        target: "https://api.flickr.com/services/feeds/photos_public.gne",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/images/, ""),
+      },
+    },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.ts",
+  },
+});
